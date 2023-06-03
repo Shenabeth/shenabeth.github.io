ce4c4c2c55f6f6bf23bebaf3ca413755e6331ef7
Shenabeth Jenkins <shenabeth@gmail.com>
2023-06-03 19:30:59 -0400

    Added dropdown arrows

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const collapsibleBtns = document.querySelectorAll('.collapsible');
 collapsibleBtns.forEach(function (button) {
   button.addEventListener('click', function () {
     const content = this.nextElementSibling;
+    const arrow = this.querySelector('.arrow');
 
     if (content.style.display === 'none' || content.style.display === '') {
       content.style.display = 'block';
+      arrow.classList.remove('fa-chevron-down');
+      arrow.classList.add('fa-chevron-right');
     } else {
       content.style.display = 'none';
+      arrow.classList.remove('fa-chevron-right');
+      arrow.classList.add('fa-chevron-down');
     }
   });
 });
