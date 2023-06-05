2578686f976e4b18cadd957949252dd446c482f9
Shenabeth Jenkins <55063760+Shenabeth@users.noreply.github.com>
2023-06-05 11:10:18 -0400

    Update layout

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const collapsibleBtns = document.querySelectorAll('.collapsible');
+// const collapsibleBtns = document.querySelectorAll('.collapsible');
+// const menuBtn = document.querySelector('.menuImg');
+// const leftSidebar = document.querySelector('.left-sidebar');
+// const rightSidebar = document.querySelector('.right-sidebar');
 
+// menuBtn.addEventListener('click', function () {
+//   leftSidebar.classList.toggle('collapsed');
+//   rightSidebar.classList.toggle('expanded');
+// });
+
+// collapsibleBtns.forEach(function (button) {
+//   button.addEventListener('click', function () {
+//     const content = this.nextElementSibling;
+//     const arrow = this.querySelector('.arrow');
+
+//     content.classList.toggle('show');
+
+//     if (content.classList.contains('show')) {
+//       arrow.classList.remove('fa-chevron-down');
+//       arrow.classList.add('fa-chevron-right');
+//     } else {
+//       arrow.classList.remove('fa-chevron-right');
+//       arrow.classList.add('fa-chevron-down');
+//     }
+//   });
+// });
+
+
+
+
+
+
+
+const collapsibleBtns = document.querySelectorAll('.collapsible');
 collapsibleBtns.forEach(function (button) {
   button.addEventListener('click', function () {
     const content = this.nextElementSibling;
