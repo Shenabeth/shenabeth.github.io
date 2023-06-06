13a84b0a791cf0a94ec10cb68ceef2ca7b6ed220
Shenabeth Jenkins <shenabeth@gmail.com>
2023-06-05 23:02:57 -0400

    major layout update

--- index.js
+++ index.js
@@ -31,20 +31,45 @@
 
 
 
-const collapsibleBtns = document.querySelectorAll('.collapsible');
-collapsibleBtns.forEach(function (button) {
-  button.addEventListener('click', function () {
-    const content = this.nextElementSibling;
-    const arrow = this.querySelector('.arrow');
+// const collapsibleBtns = document.querySelectorAll('.collapsible');
+// collapsibleBtns.forEach(function (button) {
+//   button.addEventListener('click', function () {
+//     const content = this.nextElementSibling;
+//     const arrow = this.querySelector('.arrow');
 
-    if (content.style.display === 'none' || content.style.display === '') {
-      content.style.display = 'block';
-      arrow.classList.remove('fa-chevron-down');
-      arrow.classList.add('fa-chevron-right');
-    } else {
-      content.style.display = 'none';
-      arrow.classList.remove('fa-chevron-right');
-      arrow.classList.add('fa-chevron-down');
-    }
-  });
+//     if (content.style.display === 'none' || content.style.display === '') {
+//       content.style.display = 'block';
+//       arrow.classList.remove('fa-chevron-down');
+//       arrow.classList.add('fa-chevron-right');
+//     } else {
+//       content.style.display = 'none';
+//       arrow.classList.remove('fa-chevron-right');
+//       arrow.classList.add('fa-chevron-down');
+//     }
+//   });
+// });
+
+
+
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuSidebar = document.querySelector('.menu-sidebar');
+const rightSidebar = document.querySelector('.right-sidebar');
+
+menuBtn.addEventListener('click', function () {
+  menuSidebar.classList.toggle('open');
+  rightSidebar.classList.toggle('collapsed');
+  if (menuSidebar.classList.contains('open')) {
+    menuSidebar.style.width = '13vw';
+    rightSidebar.style.width = '81vw';
+    rightSidebar.style.left = '19vw';
+    menuSidebar.classList.remove('hide-contents');
+  } else {
+    menuSidebar.style.width = '0vw';
+    rightSidebar.style.width = '96vw';
+    rightSidebar.style.left = '4vw';
+    menuSidebar.classList.add('hide-contents');
+  }
 });
+
