726d3063e747b2a35b179704de56afb142c27a26
Shenabeth Jenkins <shenabeth@gmail.com>
2023-06-06 01:11:55 -0400

    update arrows and layout

--- index.js
+++ index.js
@@ -1,56 +1,20 @@
-// const collapsibleBtns = document.querySelectorAll('.collapsible');
-// const menuBtn = document.querySelector('.menuImg');
-// const leftSidebar = document.querySelector('.left-sidebar');
-// const rightSidebar = document.querySelector('.right-sidebar');
+const collapsibleBtns = document.querySelectorAll('.collapsible');
+collapsibleBtns.forEach(function (button) {
+  button.addEventListener('click', function () {
+    const content = this.nextElementSibling;
+    const arrow = this.querySelector('.arrow');
 
-// menuBtn.addEventListener('click', function () {
-//   leftSidebar.classList.toggle('collapsed');
-//   rightSidebar.classList.toggle('expanded');
-// });
-
-// collapsibleBtns.forEach(function (button) {
-//   button.addEventListener('click', function () {
-//     const content = this.nextElementSibling;
-//     const arrow = this.querySelector('.arrow');
-
-//     content.classList.toggle('show');
-
-//     if (content.classList.contains('show')) {
-//       arrow.classList.remove('fa-chevron-down');
-//       arrow.classList.add('fa-chevron-right');
-//     } else {
-//       arrow.classList.remove('fa-chevron-right');
-//       arrow.classList.add('fa-chevron-down');
-//     }
-//   });
-// });
-
-
-
-
-
-
-
-// const collapsibleBtns = document.querySelectorAll('.collapsible');
-// collapsibleBtns.forEach(function (button) {
-//   button.addEventListener('click', function () {
-//     const content = this.nextElementSibling;
-//     const arrow = this.querySelector('.arrow');
-
-//     if (content.style.display === 'none' || content.style.display === '') {
-//       content.style.display = 'block';
-//       arrow.classList.remove('fa-chevron-down');
-//       arrow.classList.add('fa-chevron-right');
-//     } else {
-//       content.style.display = 'none';
-//       arrow.classList.remove('fa-chevron-right');
-//       arrow.classList.add('fa-chevron-down');
-//     }
-//   });
-// });
-
-
-
+    if (content.style.display === 'none' || content.style.display === '') {
+      content.style.display = 'block';
+      arrow.classList.remove('fa-chevron-down');
+      arrow.classList.add('fa-chevron-right');
+    } else {
+      content.style.display = 'none';
+      arrow.classList.remove('fa-chevron-right');
+      arrow.classList.add('fa-chevron-down');
+    }
+  });
+});
 
 
 const menuBtn = document.querySelector('.menu-btn');
@@ -72,4 +36,3 @@ menuBtn.addEventListener('click', function () {
     menuSidebar.classList.add('hide-contents');
   }
 });
-
