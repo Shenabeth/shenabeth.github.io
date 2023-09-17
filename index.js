20fbcced6367dbe3b1c062ea6d299f78e48accaa
Shenabeth Jenkins <55063760+Shenabeth@users.noreply.github.com>
2023-09-17 13:54:41 -0400

    new website files

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var menuToggle = document.querySelector('.menu-toggle');
-  var menuItems = document.querySelector('.menu');
+// document.addEventListener('DOMContentLoaded', function () {
+//   var menuToggle = document.querySelector('.menu-toggle');
+//   var menuItems = document.querySelector('.menu');
 
-  menuToggle.addEventListener('click', function () {
-    menuItems.classList.toggle('show');
-  });
+//   menuToggle.addEventListener('click', function () {
+//     menuItems.classList.toggle('show');
+//   });
 
   // Close the menu when a menu item is clicked
-  var menuLinks = document.querySelectorAll('.menu a');
+//   var menuLinks = document.querySelectorAll('.menu a');
 
-  menuLinks.forEach(function (link) {
-    link.addEventListener('click', function () {
-      menuItems.classList.remove('show');
-    });
-  });
-});
+//   menuLinks.forEach(function (link) {
+//     link.addEventListener('click', function () {
+//       menuItems.classList.remove('show');
+//     });
+//   });
+// });
